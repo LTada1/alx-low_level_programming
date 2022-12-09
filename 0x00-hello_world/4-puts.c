b84ce339a61d6d60fdbf8938cd98bc492dc21a91
LTada1 <lukaharuna4@gmail.com>
2022-12-09 15:37:08 -0800

    Using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *main- Entry point
+ *puts - print some strings
+ *@_prog : contains strings
+ *
+ *Description: this program prints output ising puts
+ *
+ *Return: Always 0 success
+ */
+
+int main(void)
+{
+	char _prog[]="Programming is like building a multilingual puzzle"; 
+		 
+	puts(_prog);
+	return (0);
+}
