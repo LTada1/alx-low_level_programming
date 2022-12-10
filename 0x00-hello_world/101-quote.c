c94470a7c1a3107bac9cd709b3c592722c82daae
LTada1 <lukaharuna4@gmail.com>
2022-12-10 06:18:02 -0800

    Using unistd.h library

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
-#include<stdio>
+#include<stdio.h>
 #include<unistd.h>
 
 /**
  * main- Entry point
  * write: Prints string
  *
- * Return:1 success
+ * Return:Always 0 success
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59;
+	write(2,"and that piece of art is useful\" -Dora korpar, 2015-10-19\n",59);
 	return (1);
 
 }
