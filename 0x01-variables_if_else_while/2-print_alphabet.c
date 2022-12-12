851377225ca02df36f28ed2b6eb67c6f76b7b15a
LTada1 <lukaharuna4@gmail.com>
2022-12-11 18:21:27 -0800

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
-#include<stdio>
+#include<stdio.h>
 
 /**
  * main- Entry point
- *
- *
+ *putchar:prints alphabet in lowercase
+ *al:is a variable
  *
  * Return:Always 0 success
  *
@@ -11,5 +11,13 @@
 
 int main(void)
 {
+	char al = 'a';
+	while (al< 'z')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+	return (0);
 
 }
