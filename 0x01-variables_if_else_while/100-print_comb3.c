5f20b1a65efff847042d287434cd6be07aa57e01
LTada1 <lukaharuna4@gmail.com>
2022-12-12 17:47:31 -0800

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main-Entry Point
+ * main - prints a list of double didgits
  *
- *
- *
- *
- * Retrun:Always 0 success
- */
+ * Description: digits should not be repeated twice
+ * Return: Always(0) Success
+*/
 
 int main(void)
 {
+	int i, j;
 
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
+	return (0);
 }
