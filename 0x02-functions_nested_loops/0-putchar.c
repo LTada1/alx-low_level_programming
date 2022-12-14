4bd43f36c9f89aab0fa046ee7563db7198ae288d
LTada1 <lukaharuna4@gmail.com>
2022-12-14 06:30:25 -0800

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main -Entry Point
+ * printf: print string
+ * ch:variable
+ *
+ *
+ * Return:Always 0 success
+ */
+
+int main(void)
+{
+	char ch[] = "_putchar";
+
+	printf("%s\n", ch);
+	return (0);
+}
