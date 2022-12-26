f177e8478ddbd1bf1f00eac9ab32b66483404846
LTada1 <lukaharuna4@gmail.com>
2022-12-26 03:55:30 -0800

    prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main-Entry Point
+ * print_alphabet
+ *
+ * Description: This program print alphabets a-z,
+ * and a new line
+ *
+ * Return:Always 0 success
+ */
+
+void print_alphabet(void)
+{
+	char lu;
+	
+	for (lu = 'a'; lu <= 'z'; lu++)
+	{
+		_putchar(lu);
+	}
 	_putchar('\n');
+}
