ac8d655aa49d50f22f7ebfcb170101b74033b951
LTada1 <lukaharuna4@gmail.com>
2022-12-27 01:15:50 -0800

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@ int print_sign(int n)
 {
 if (n > 48)
 {
-_putchar(43);;
+_putchar(43);
 return (1);
 }
-else if (n == 48)
+else if (n = 48)
 {
- _putchar(48);
+_putchar(48);
 return (0);
 }
 else if (n < 48)
