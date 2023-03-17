b4d9b614222e3690f6fe41d4525920b9195878b2
LTada1 <lukahruna4@gmail.com>
2023-03-16 23:33:09 -0700

    Sizeof Opeartor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,28 +3,18 @@
 /**
  * main - Entry Point
  * printf - Print strings
- * @char: Varibale
- * @int: Variable
- * @long: Variable
- * @lon long: Variable
- * @float: Varible
+ * @sizeof: Sizeof opeartor
  *
  * Return: Always 0 success
  */
 
- int main(void)
- {
- 	char;
-	int;
-	long int;
-	long long int;
-	float;
-
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
