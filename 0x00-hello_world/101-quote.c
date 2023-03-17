9bc0fce0ea7f2026284d12386958b7af70834088
LTada1 <lukahruna4@gmail.com>
2023-03-17 00:04:09 -0700

    Using the putchar function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * putchar: Prints one character at a time
+ * @str: Variable
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str [] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+
+	putchar(str);
+	putchar(\n);
+
+	return (1);
+}
