23a2589f4b799a9861ac4b7fcf860c04a888d6a0
LTada1 <lukahruna4@gmail.com>
2023-03-18 12:20:27 -0700

    Print numbers only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+/**
+ * main - Entry point
+ * putchar: Print Character one at a time
+ * Return: Always 0 Success
+ */
+ 
+int main(void)
+{
+	char num_b;
+
+	num_b = 48;
+
+	while (num_b <= 57)
+	{
+		putchar(num_b);
+		num_b++;
+	}
+	putchar('\n');
+
+	return (0);
+}
