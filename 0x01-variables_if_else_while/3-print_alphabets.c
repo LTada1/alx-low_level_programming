ff74105c1fea41357b6206a5c99c07af9e0d9801
LTada1 <lukahruna4@gmail.com>
2023-03-19 06:14:36 -0700

    Betty coding style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,26 +4,26 @@
  * putchar: Print Character one at a time
  * Return: Always 0 Success
  */
-  
+
 int main(void)
 {
-          char abc;
-	  char ABC;
+	char abc;
+	char ABC;
 
-	  abc = 'a';
-	  ABC = 'A';
-	  while (ABC <= 'Z')
-          {
-		  if (abc != 'z')
-			  while (abc <= 'z')
-			  {
-				  putchar(abc);
-				  abc++;
-			  }
-		  putchar(ABC);
-		  ABC++;
-          }
-          putchar('\n');
- 
-          return (0);
+	abc = 'a';
+	ABC = 'A';
+	while (ABC <= 'Z')
+	{
+		if (abc != 'z')
+			while (abc <= 'z')
+			{
+				putchar(abc);
+				abc++;
+			}
+		putchar(ABC);
+		ABC++;
+	}
+	putchar('\n');
+
+	return (0);
 }
