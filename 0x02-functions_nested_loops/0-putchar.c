f6bcc95d2190bae4ad95c815fc0b83900752a78c
LTada1 <lukahruna4@gmail.com>
2023-03-21 15:56:28 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 #include "main.h"
 /**
  * main - Entry point
- * putchar: Print Character one at a time
+ * _putchar: Print Character one at a time
  * Return: Always 0 Success
  */
 
@@ -12,12 +12,12 @@ int main(void)
 	char str[] = "_putchar";
 	unsigned int i = 0;
 
-	while (i <= strlen(str))
+	while (i < strlen(str))
 	{
 		_putchar(str[i]);
 		i++;
 	}
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
