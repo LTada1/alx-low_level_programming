374b1b01ca65afc55758d9090c38a6178d89f15f
LTada1 <lukahruna4@gmail.com>
2023-03-22 05:33:26 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * add - add two numbers
+  * @x: Variable
+  * @y: Variable 2
+  * Return: Always absolute
+  */
+
+int add(int x, int y)
+{
+	int add;
+
+	add = (x + y);
+	return (add);
+}
