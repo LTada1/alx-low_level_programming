34e57571dac5d35b59c9c0e30aaf637314ffa10d
LTada1 <lukahruna4@gmail.com>
2023-03-23 20:53:18 -0700

    Postive or negative check for a number

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry Point
- * printf: prints strings
- * Return: Always 0 success
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,20 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
+/**
+ * positive_or_negative - Test for postive or negative number
+ * @i: Variable
+ * Return: Always 0 success
+ */
 
-/* betty style doc for function main goes there */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n ==0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i ==0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is negative\n", n);
-	return (0);
+		printf("%d is negative\n", i);
 }
