ff3ef88b3c4bec4e697496560d9b64713e457a2e
LTada1 <lukahruna4@gmail.com>
2023-03-23 17:25:14 -0700

    computes product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers togerther
+ * @a: Variable one
+ * @b: Variable two
+ * Return: Always absolute
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
