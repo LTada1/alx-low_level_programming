ad39873a78768b8caaf17132f986e675e0ea5ea7
LTada1 <lukahruna4@gmail.com>
2023-04-10 18:55:05 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,55 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
- * main - prints program name
- * @argc: number of arguments
- * @argv: argurment vector
+ * digitcheck - check digit in string
+ * @str: string arguments
  *
  * Return: On success 0.
  */
+int digitcheck(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+			return (0);
+	}
+	return (1);
+}
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: count number of arguments
+ * @argv: number of strings
+ *
+ * Return: on 0 success.
+ */
+
 int main(int argc, char *argv[])
 {
-	int ad_d, i;
+	int ad_d, i, x;
 
 	ad_d = 0;
-	(void) argc;
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (digitcheck(argv[i]))
 		{
-			int x = strtol(argv[i], NULL, 10);
-			char y = 48;
-
-			if (x  >= y)
+			x = strtol(argv[i], NULL, 10);
 			ad_d += x;
-			else
-				printf("Error\n");
 		}
-		printf("%d\n", ad_d);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
-	else
-		printf("0\n");
-	return (1);
+	printf("%d\n", ad_d);
+
+	return (0);
 }
