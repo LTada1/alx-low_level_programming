15178493beb5f0cb725f367d6bb8aab34679e0c3
LTada1 <lukahruna4@gmail.com>
2023-04-14 19:02:26 -0700

    void pointer problem

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *array;
+	void *AR;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	array = calloc(nmemb, sizeof(size));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb; ++i)
+	AR = malloc(nmemb * size);
+	if (AR == NULL)
 	{
-		array[i] = i + 1;
+		return (NULL);
 	}
-	return (array);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)(AR) + i) = 0;
+	}
+	return (AR);
 }
