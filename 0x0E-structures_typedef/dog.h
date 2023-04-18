a35d6fb015617bcb264a363c7af2b52a5bf691ad
LTada1 <lukahruna4@gmail.com>
2023-04-18 16:00:35 -0700

    new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A user defined data type
+ * @name: member
+ * @age: member
+ * @owner: member
+ *
+ * Gives information about a particular dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
