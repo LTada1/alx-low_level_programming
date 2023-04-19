90a78caebf71949dc23c8a23c6dce9cf83a9ea96
LTada1 <lukahruna4@gmail.com>
2023-04-18 18:23:27 -0700

    Betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,8 +9,6 @@
  *
  * Return: .
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)");
-	else
-	printf("Name: %s\n", d->name);
-	if (d->age == 0.0)
-		printf("Age: (nil)");
-	else
-	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
-	else
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,d->age,d->owner);
 }
