cadc1e151a6879b5b673e69a61ee82d801ce32b3
LTada1 <lukahruna4@gmail.com>
2023-04-18 21:45:35 -0700

    Incorrect output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)");
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,d->age,d->owner);
+	if (d->age == 0.0)
+		printf("Age: (nil)");
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
