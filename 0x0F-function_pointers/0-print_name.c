b2e7aaa08f19e4c7bd91ab642b7cc4e9c9a56fdc
LTada1 <lukahruna4@gmail.com>
2023-04-20 19:13:03 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+ * print_name - Print name
+ * @name: pointer toparameters
+ * @f: a pointer to a function
+ *
+ * Return: null.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
