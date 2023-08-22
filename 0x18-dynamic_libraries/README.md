b11ddd4050a4d40364939fbe50551ce7d3515cc9
Ltada1 <lukaharua4@gmail.com>
2023-08-21 17:13:39 -0700

    Shared libraries

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,2 @@
+This project is about C dynamic or shared library.
+    It shows how to crate and use dynamic library in c.
---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char _abs(char *str);
+char _atoi(char *str);
+char __bss_start(char *str);
+char __cxa_finalize(char *str);
+char _edata(char *str);
+char _end(char *str);
+char _fini(char *str);
+char __gmon_start__(char *str);
+int _init(int a, int b);
+char _isalpha(char *str);
+char _isdigit(char *str);
+char _islower(char *str);
+char _isupper(char *str);
+char _ITM_deregisterTMCloneTable(char *str);
+char _ITM_registerTMCloneTable(char *str);
+char _Jv_RegisterClasses(char *str);
+char _memcpy(char *str);
+char _memset(char *strc);
+char _putchar(char *str);
+char _puts(char *str);
+char _strcat(char *str);
+char _strchr(char *str);
+char _strcmp(char *stra, char *strb);
+char _strcpy(char *stra, char *strb);
+int _strlen(char *str);
+char _strncat(char *stra, char *strb);
+char _strncpy(char *stra, char *strb);
+char _strpbrk(char *str);
+char _strspn(char *str);
+char _strstr(char *str);
+
+#endif
