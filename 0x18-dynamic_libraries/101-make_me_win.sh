252c06bf265216f8484a11b94aee14bbf3f1f603
Ltada1 <lukaharua4@gmail.com>
2023-08-25 20:30:10 -0700

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+gcc -shared -o printit.so -fPIC jackpot.c
+export LD_PRELOAD=./printit.so:$LD_LIBRARY_PATH
---
+++ 0x18-dynamic_libraries/jackpot.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int print(const char formart, ...)
+{
+    write(1, "8, 10, 74\n",17);
+    write(1, "conragtulation\n",38);
+    exit(EXIT_SUCCESS);
+}
