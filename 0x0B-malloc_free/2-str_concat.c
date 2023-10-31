136c03bf94cc18825a8e0040681da514e8b649a6
LTada1 <lukaharuna4@gmail.com>
2023-10-31 07:33:40 -0700

    tough

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,14 +18,6 @@ char *str_concat(char *s1, char *s2)
 	int m;
 	char *dest;
 
-	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
-	if (s2 == NULL)
-	{
-		s2 = " ";
-	}
 	n = strlen(s1);
 	m = strlen(s2);
 	dest = (char *)malloc(sizeof(char) * (n + m + 1));
@@ -34,6 +26,10 @@ char *str_concat(char *s1, char *s2)
 		printf("failed to allocate memory\n");
 		return (NULL);
 	}
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
 	for (i = 0; i < m && s1[i] != 0; i++)
 	{
 		dest[i] = s1[i];
@@ -41,6 +37,10 @@ char *str_concat(char *s1, char *s2)
 	for (; i < n; i++)
 	{
 		dest[i] = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
 	}
 	for (i = 0; s2[i]; i++)
 	{
