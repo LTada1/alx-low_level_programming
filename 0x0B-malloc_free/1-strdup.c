ea7c4f47a711212b044079856c1c14b4431ed6d7
LTada1 <lukaharuna4@gmail.com>
2023-10-31 05:21:13 -0700

    copy exactly

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: size of array
+ *
+ * Return: returns a pointer to a newly allocated space in memory
+*/
+
+char *_strdup(char *str)
+{
+	char *dup_cate;
+	unsigned int n;
+	unsigned int i;
+	n = strlen(str);
+
+	if (str == NULL)
+	{
+		return(NULL);
+	}
+	dup_cate = (char *)malloc(sizeof(char) * n);
+	if (dup_cate == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < n && str[i] != 0; i++)
+	{
+		dup_cate[i] = str[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dup_cate[i] = '\0';
+	}
+	return (dup_cate);
+	free(dup_cate);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,5 @@
 #define MALLOC_FEE
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
