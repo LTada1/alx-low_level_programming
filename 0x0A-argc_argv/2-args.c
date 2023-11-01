c4c76307184965aaa2cb11a4a3b437633ebf9412
LTada1 <lukaharuna4@gmail.com>
2023-11-01 08:03:50 -0700

    indent

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 int i;
 if (argc > 0)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+{
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+}
 else
-	printf("No argument found\n");
+printf("No argument found\n");
 return (0);
 }
