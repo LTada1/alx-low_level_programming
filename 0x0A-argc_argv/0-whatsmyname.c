8a1650b2dd8002382d25b37903c0910b45be047d
LTada1 <lukaharuna4@gmail.com>
2023-11-01 07:38:18 -0700

    say my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of charcters
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
