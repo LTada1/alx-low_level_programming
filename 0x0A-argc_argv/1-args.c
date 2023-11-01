4dc186e3cb3b8e3ff9defd30238a6d26acad9c33
LTada1 <lukaharuna4@gmail.com>
2023-11-01 07:48:02 -0700

    number of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of charcters
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
