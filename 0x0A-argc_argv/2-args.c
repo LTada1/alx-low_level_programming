0bedf746e5356defd6772cf157413dc7f556de8d
LTada1 <lukaharuna4@gmail.com>
2023-11-01 08:01:58 -0700

    perfect

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of charcters
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+else
+	printf("No argument found\n");
+return (0);
+}
