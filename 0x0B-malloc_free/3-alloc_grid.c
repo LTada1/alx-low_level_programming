9c848a050b7d9ad15175f24c17951fd61afd1a8d
LTada1 <lukaharuna4@gmail.com>
2023-11-01 17:50:45 -0700

    always free memory when malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 /**
- *  *_strcat - concatenates two strings.
- * @dest: Pointer Variable
- * @src: Pointer Variable
+ *  alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: Num of columns
+ * @height: num of rows
  *
- * Return: Always 0.
+ * Return: returns a pointer.
  */
 
 int **alloc_grid(int width, int height)
@@ -21,10 +21,11 @@ if ((width == 0 || height == 0) || (width < 0 || height < 0))
 {
 return (NULL);
 }
-array = (int**)malloc(sizeof(int*) * height);
+array = (int **)malloc(sizeof(int *) * height);
 if (array == NULL)
 {
 return (NULL);
+free(array);
 }
 for (i = 0; i < height; i++)
 {
@@ -32,7 +33,7 @@ array[i] = malloc(sizeof(int) * width);
 }
 for (i = 0; i < height; i++)
 {
-for(j = 1; j < width; j++)
+for (j = 1; j < width; j++)
 {
 array[i][j] = 0;
 }
