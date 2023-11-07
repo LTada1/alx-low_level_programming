3d79a21b315fc6d7673676a30bfdb899fc517fc7
LTada1 <lukaharuna4@gmail.com>
2023-11-06 16:53:11 -0800

    perfection

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * struct dog - struct variable
+ *
  * @name: element of struct dog
  * @age: element of struct dog
  * @owner: element of struct dog
+ *
+ * Description: This is struct of type dog.
  */
 
 struct dog
