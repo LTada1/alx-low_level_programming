db7731536db9782f85b0b221360ee0a345d37094
LTada1 <lukaharuna4@gmail.com>
2023-11-06 16:32:42 -0800

    Only free dynamic allocated memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,24 +16,27 @@ int i, len1, len2;
 
 len1 = strlen(name);
 len2 = strlen(owner);
-
 dog_n = malloc(sizeof(struct dog));
 if (dog_n == NULL)
+return (NULL);
+dog_n->name = malloc(sizeof(char) * (len1  + 1));
+if (dog_n->name == NULL)
 {
+free(dog_n);
 return (NULL);
 }
-
-dog_n->name = malloc(sizeof(char) * (len1  + 1));
 dog_n->owner = malloc(sizeof(char) * (len2 + 1));
-for (i = 0; i < len1; i++)
+if (dog_n->owner == NULL)
 {
-dog_n->name[i] = name[i];
+free(dog_n);
+free(dog_n->name);
+return (NULL);
 }
+for (i = 0; i < len1; i++)
+	dog_n->name[i] = name[i];
 dog_n->name[i] = '\0';
 for (i = 0; i < len2; i++)
-{
-dog_n->owner[i] = owner[i];
-}
+	dog_n->owner[i] = owner[i];
 dog_n->owner[i] = '\0';
 dog_n->age = age;
 return (dog_n);
