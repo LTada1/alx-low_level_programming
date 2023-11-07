de19ac9338c92fdbb0a41519fcf0fe5f9ed9966e
LTada1 <lukaharuna4@gmail.com>
2023-11-06 16:01:30 -0800

    new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+#include <string.h>
 /**
  * new_dog - create new struct
  * @name: member of struct
@@ -9,18 +9,32 @@
  * @age: member of struct
  * Return: Does not return;
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 struct dog *dog_n;
+int i, len1, len2;
+
+len1 = strlen(name);
+len2 = strlen(owner);
 
 dog_n = malloc(sizeof(struct dog));
 if (dog_n == NULL)
 {
 return (NULL);
 }
-dog_n->name = name;
-dog_n->owner = owner;
+
+dog_n->name = malloc(sizeof(char) * (len1  + 1));
+dog_n->owner = malloc(sizeof(char) * (len2 + 1));
+for (i = 0; i < len1; i++)
+{
+dog_n->name[i] = name[i];
+}
+dog_n->name[i] = '\0';
+for (i = 0; i < len2; i++)
+{
+dog_n->owner[i] = owner[i];
+}
+dog_n->owner[i] = '\0';
 dog_n->age = age;
 return (dog_n);
 }
