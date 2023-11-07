d97eb2ea388926fe189bcf5f02a5b71f6e1577cd
LTada1 <lukaharuna4@gmail.com>
2023-11-06 17:04:50 -0800

    confused

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
 /**
- * struct dog - struct variable
+ * struct dog - a struct type for dog
  *
- * @name: element of struct dog
- * @age: element of struct dog
- * @owner: element of struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Description: This is struct of type dog.
+ * Description: This is struct of type dog
  */
 
 struct dog
