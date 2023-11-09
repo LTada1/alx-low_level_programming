5ce7f7bcc78c01874eb96772d67ca57a6707acbc
LTada1 <lukaharuna4@gmail.com>
2023-11-08 16:24:28 -0800

    name change

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints using a pointer to a function as a parameter,
+ * it returns a pointer to a function.
+ * @f: function pointer
+ * @name: string variable
+ *
+ * Return: Does not return;
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
