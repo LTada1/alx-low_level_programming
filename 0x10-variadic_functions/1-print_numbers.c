6447cfb843f2bfed06031bad57b2ed158d9f0714
LTada1 <lukaharuna4@gmail.com>
2023-11-09 08:01:09 -0800

    always va_end!

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,18 +15,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list var_ar;
 unsigned int i;
 
-if (n == 0 || separator == NULL)
+if (n == 0)
 {
 return;
 }
 va_start(var_ar, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(var_ar, int));
-if (separator == NULL)
-continue;
+printf("%d", va_arg(var_ar, unsigned int));
 if (i != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
+va_end(var_ar);
 }
