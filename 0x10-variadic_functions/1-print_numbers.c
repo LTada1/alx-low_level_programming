b40f7c2523af1c95ac3de37f11bfa4c670a85bb7
LTada1 <lukaharuna4@gmail.com>
2023-11-09 07:51:10 -0800

    print integers from optional paremeters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - Print integers from optional parameters
+ * @n: function parameter
+ * @separator: string variable
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list var_ar;
+unsigned int i;
+
+if (n == 0)
+{
+return;
+}
+va_start(var_ar, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(var_ar, int));
+if (separator == NULL)
+continue;
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+}
