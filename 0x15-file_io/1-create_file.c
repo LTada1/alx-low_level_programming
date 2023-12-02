ecdfe1bc6167ffa93f50f9bc3686dd208a8045c8
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-02 04:58:53 +0100

    My bad

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,14 @@
 * Return: 1 on success & 0 on failure.
 */
 
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 int file_descriptor;
 int character_count;
 int write_result;
 
-if (!file_name) { return -1; }
+if (!file_name)
+return -1;
 
 file_descriptor = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
@@ -27,16 +29,15 @@ if (!content)
   content = "";
 }
 
-for (character_count = 0; content[character_count]; character_count++)
-;
-
+for (character_count = 0; content[character_count]; character_count++);
+{
 write_result = write(file_descriptor, content, character_count);
 
 if (write_result == -1)
 { 
  return (-1);
 }
-
+}
 close(file_descriptor);
 
 return (1);
