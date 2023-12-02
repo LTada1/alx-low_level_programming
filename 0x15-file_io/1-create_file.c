f1e48f3e48b78ff4da8163046e07ed9430676eb5
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-02 05:43:37 +0100

    No rush

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,25 +20,18 @@ return (-1);
 file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 if (file_descriptor == -1)
-{
-  return (-1);
-}
+return (-1);
 
 if (!text_content)
-{
-  text_content = "";
-}
+text_content = "";
 
 for (character_count = 0; text_content[character_count]; character_count++);
 
 write_result = write(file_descriptor, text_content, character_count);
 
 if (write_result == -1)
-{ 
- return (-1);
-}
+return (-1);
 
 close(file_descriptor);
-
 return (1);
 }
