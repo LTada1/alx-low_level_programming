9135b68a172cf0b308fdb70fbfad2027d9bc9630
Lukatada <111013434+LTada1@users.noreply.github.com>
2023-12-02 03:43:12 +0100

    All good

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,8 +9,7 @@
 * Return: void
 */
 
-
-void file_copy(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 if (file_from == -1)
 {
@@ -31,12 +30,11 @@ exit(99);
 * Return: 0 on success
 */
 
-
 int main(int argc, char *argv[])
 {
 int file_from, stop_error, file_to;
-ssize_t i, j;
-char BUFF[1024];
+ssize_t tally, filewr;
+char pbuffer[1024];
 
 if (argc != 3)
 {
@@ -46,16 +44,16 @@ exit(97);
 
 file_from = open(argv[1], O_RDONLY);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-file_copyfile_from, file_to, argv);
+error_file(file_from, file_to, argv);
 
-i = 1024;
-while (i == 1024)
+tally = 1024;
+while (tally == 1024)
 {
-i = read(file_from, BUFF, 1024);
-if (i == -1)
+tally = read(file_from, pbuffer, 1024);
+if (tally == -1)
 error_file(-1, 0, argv);
-j = write(file_to, BUFF, i);
-if (j == -1)
+filewr = write(file_to, pbuffer, tally);
+if (filewr == -1)
 error_file(0, -1, argv);
 }
 
